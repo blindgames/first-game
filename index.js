f9bfcb0b3022999af036ab55d8c066e26d83c0b4
blindgames <152272523+blindgames@users.noreply.github.com>
2023-11-28 13:20:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const vid = document.getElementById("ses");
  const ac = document.getElementById("ac");
  const aci = document.getElementById("aci");
  const kgir = document.getElementById("k-gir")
+ const ksev = document.getElementById("k-sev")
 function basla(){
     alert("    görme engelli kullanıcılar için özel yapım RPG oyunu dikkat çok eğlencelidir")
     vid.play();
@@ -14,5 +15,9 @@ function gir(){
     aci.style.display = "block";
 
 }
+function ksevs(){
+    alert("Oyun bitti tebrikler kahraman!");
+}
 kgir.addEventListener("click",gir)
 btn.addEventListener("click",basla)
+ksev.addEventListener("click",ksevs)
