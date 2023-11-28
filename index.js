dcd31b4922fae5b82fb7101c81dbffe4505700de
blindgames <152272523+blindgames@users.noreply.github.com>
2023-11-28 14:42:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const vid = document.getElementById("ses");
  const aci = document.getElementById("aci");
  const kgir = document.getElementById("k-gir")
  const ksev = document.getElementById("k-sev")
+ const adam = document.getElementById("adam")
+ const kkurtar = document.getElementById("k-kurtar");
+ const kkurtarma = document.getElementById("k-kurtarma");
 function basla(){
-    alert("    görme engelli kullanıcılar için özel yapım RPG oyunu dikkat çok eğlencelidir")
+    alert("görme engelli kullanıcılar için özel yapım RPG oyunu dikkat çok eğlencelidir")
     vid.play();
-    ac.style.display = "block";
-}
+ }
 
 function gir(){
     alert("Köye girdin!")
@@ -16,8 +18,19 @@ function gir(){
 
 }
 function ksevs(){
-    alert("Oyun bitti tebrikler kahraman!");
+    alert("100 tane kedi geldi gelin yemeğimiz ayağımıza geldi dedi kedilerden kaçtın");
+    aci.style.display= "none"
+    adam.style.display ="block";
+ }
+ function kKurtar(){
+    alert("adamı kurtarıyorsun bunu yaptığın için adam sana fedakarlik ödülü olarak bir altın kılıç verdi bu kılıç hasarını 5 den 100 e çıkardı artık güçlüsün")
+    adam.style.display = "none";
 }
+ function kKurtarma(){
+    alert("adam öldü")
+ }
 kgir.addEventListener("click",gir)
 btn.addEventListener("click",basla)
 ksev.addEventListener("click",ksevs)
+kkurtar.addEventListener("click",kKurtar)
+kkurtarma.addEventListener("click",kKurtarma)
